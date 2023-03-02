28c285a6312b2bab418a7715807ee55a4258a0c6
jjdone <ehdlfaos@gmail.com>
2023-03-02 17:17:01 +0900

    Add User domain

--- src/main/java/simple/mentoring/domain/User.java
+++ src/main/java/simple/mentoring/domain/User.java
@@ -1,2 +1,53 @@
-package simple.mentoring.domain;public class User {
+package simple.mentoring.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import simple.mentoring.configuration.Qualification;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String loginId;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true, nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private Qualification qualification;
+
+    private String phone;
+    private LocalDateTime createdDate;
+    private LocalDateTime modifiedDate;
+
+    @PrePersist
+    public void createDate() {
+        this.createdDate = LocalDateTime.now();
+    }
+
+    @Builder
+    public User(String loginId, String password, String email, String nickname, Qualification qualification, String phone) {
+        this.loginId = loginId;
+        this.password = password;
+        this.email = email;
+        this.nickname = nickname;
+        this.qualification = qualification;
+        this.phone = phone;
+    }
 }
