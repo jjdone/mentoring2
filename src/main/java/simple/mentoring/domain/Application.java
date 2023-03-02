3b7f543a1bd5d6f9c725b0b39aae8447ff75aa60
jjdone <ehdlfaos@gmail.com>
2023-03-02 17:57:08 +0900

    Add Application domain

--- src/main/java/simple/mentoring/domain/Application.java
+++ src/main/java/simple/mentoring/domain/Application.java
@@ -1,2 +1,37 @@
-package simple.mentoring.domain;public class Application {
+package simple.mentoring.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import simple.mentoring.configuration.ApplicationState;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.*;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "apply_uk",
+                        columnNames = {"mentee_id", "post_id"}
+                )
+        }
+)
+public class Application {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "mentee_id", nullable = false)
+    private User mentee;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    private ApplicationState state;
 }
