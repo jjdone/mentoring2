81b1a552d3da74d351960523d405f3ec4e152581
jjdone <ehdlfaos@gmail.com>
2023-03-04 22:32:53 +0900

    Set basic MainController

--- src/main/java/simple/mentoring/controller/MainController.java
+++ src/main/java/simple/mentoring/controller/MainController.java
@@ -1,2 +1,18 @@
-package simple.mentoring.controller;public class MainController {
+package simple.mentoring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String mainForm() {
+        return "mainForm";
+    }
+
+    @GetMapping("/login")
+    public String loginForm() {
+        return "loginForm";
+    }
 }
