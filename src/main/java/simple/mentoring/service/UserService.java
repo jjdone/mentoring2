399058e7649421719b19d2e39b98ff36ef1ca106
jjdone <ehdlfaos@gmail.com>
2023-03-06 17:20:19 +0900

    Add join function

--- src/main/java/simple/mentoring/service/UserService.java
+++ src/main/java/simple/mentoring/service/UserService.java
@@ -1,2 +1,35 @@
-package simple.mentoring.service;public class UserService {
+package simple.mentoring.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import simple.mentoring.domain.User;
+import simple.mentoring.dto.user.UserSignupDto;
+import simple.mentoring.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Transactional
+    public User join(UserSignupDto userSignupDto) {
+
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+        User user = User.builder()
+                .loginId(userSignupDto.getLoginId())
+                .password(encoder.encode(userSignupDto.getPassword()))
+                .email(userSignupDto.getEmail())
+                .nickname(userSignupDto.getNickname())
+                .phone(userSignupDto.getPhone())
+                .qualification(userSignupDto.getQualification())
+                .build();
+
+        return userRepository.save(user);
+    }
+
+
 }
