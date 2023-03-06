c89fdd634729a204d46a0a673b19ae4e13e593cc
jjdone <ehdlfaos@gmail.com>
2023-03-06 17:26:07 +0900

    Add find by loginId function

--- src/main/java/simple/mentoring/repository/UserRepository.java
+++ src/main/java/simple/mentoring/repository/UserRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import simple.mentoring.domain.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByLoginId(String loginId);
 }
