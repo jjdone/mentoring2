656bfad3e9debc159d5dd76e6b08cd0dd6683fe1
jjdone <ehdlfaos@gmail.com>
2023-03-06 17:22:32 +0900

    Add signupForm controller and fix loginForm controller

--- src/main/java/simple/mentoring/controller/MainController.java
+++ src/main/java/simple/mentoring/controller/MainController.java
@@ -1,7 +1,12 @@
 package simple.mentoring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import simple.mentoring.domain.Qualification;
+import simple.mentoring.dto.user.UserLoginDto;
+import simple.mentoring.dto.user.UserSignupDto;
 
 @Controller
 public class MainController {
@@ -12,7 +17,19 @@ public class MainController {
     }
 
     @GetMapping("/login")
-    public String loginForm() {
+    public String loginForm(Model model) {
+        model.addAttribute("form", new UserLoginDto());
         return "loginForm";
+    }
+
+    @GetMapping("/signup")
+    public String signupForm(Model model) {
+        model.addAttribute("form", new UserSignupDto());
+        return "signupForm";
+    }
+
+    @ModelAttribute("qualification")
+    public Qualification[] qualifications() {
+        return Qualification.values();
     }
 }
