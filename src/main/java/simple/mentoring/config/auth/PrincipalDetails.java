ad2c4b132389afba35c636c9bb6ac4023e721720
jjdone <ehdlfaos@gmail.com>
2023-03-06 17:26:50 +0900

    Login function
    Set sesstion and userDetails

--- src/main/java/simple/mentoring/config/auth/PrincipalDetails.java
+++ src/main/java/simple/mentoring/config/auth/PrincipalDetails.java
@@ -3,11 +3,18 @@ package simple.mentoring.config.auth;
 import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import simple.mentoring.domain.User;
 
 import java.util.Collection;
 
 @Data
 public class PrincipalDetails implements UserDetails {
+
+    private User user;
+
+    public PrincipalDetails(User user) {
+        this.user = user;
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
@@ -16,31 +23,31 @@ public class PrincipalDetails implements UserDetails {
 
     @Override
     public String getPassword() {
-        return null;
+        return user.getPassword();
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return user.getLoginId();
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
--- src/main/java/simple/mentoring/config/auth/PrincipalDetailsService.java
+++ src/main/java/simple/mentoring/config/auth/PrincipalDetailsService.java
@@ -5,13 +5,21 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import simple.mentoring.domain.User;
+import simple.mentoring.repository.UserRepository;
 
 @Service
 @RequiredArgsConstructor
 public class PrincipalDetailsService implements UserDetailsService {
 
+    private final UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByLoginId(username);
+        if (user != null) {
+            return new PrincipalDetails(user);
+        }
         return null;
     }
 }
