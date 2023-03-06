a5293c93b7cd8eba170de969294106f6c9b8ff0c
jjdone <ehdlfaos@gmail.com>
2023-03-06 17:22:58 +0900

    Add sign up controller

--- src/main/java/simple/mentoring/controller/AccountController.java
+++ src/main/java/simple/mentoring/controller/AccountController.java
@@ -1,2 +1,21 @@
-package simple.mentoring.controller;public class AccountController {
+package simple.mentoring.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import simple.mentoring.dto.user.UserSignupDto;
+import simple.mentoring.service.UserService;
+
+@Controller
+@RequiredArgsConstructor
+public class AccountController {
+
+    private final UserService userService;
+
+    @PostMapping("/signup")
+    public String signup(@ModelAttribute UserSignupDto userSignupDto) {
+        userService.join(userSignupDto);
+        return "redirect:/login";
+    }
 }
