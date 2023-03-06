b2831b22d0b122724c8f6929e498d6875b4d1851
jjdone <ehdlfaos@gmail.com>
2023-03-06 17:19:31 +0900

    fix: Add Korean name

--- src/main/java/simple/mentoring/domain/Qualification.java
+++ src/main/java/simple/mentoring/domain/Qualification.java
@@ -1,5 +1,19 @@
 package simple.mentoring.domain;
 
 public enum Qualification {
-    MENTEE, MENTOR
+    MENTEE("멘티"),
+    MENTOR("멘토");
+
+    private String krName;
+
+    private Qualification() {
+    }
+
+    private Qualification(String krName) {
+        this.krName = krName;
+    }
+
+    public String getKrName() {
+        return krName;
+    }
 }
