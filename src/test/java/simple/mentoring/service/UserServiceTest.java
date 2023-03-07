78e4900c55c83d54954013f7a6d26b3fcf6a8abf
jjdone <ehdlfaos@gmail.com>
2023-03-07 13:28:55 +0900

    test: user join function test

--- src/test/java/simple/mentoring/service/UserServiceTest.java
+++ src/test/java/simple/mentoring/service/UserServiceTest.java
@@ -1,4 +1,54 @@
-import static org.junit.jupiter.api.Assertions.*;
+package simple.mentoring.service;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import simple.mentoring.domain.Qualification;
+import simple.mentoring.domain.Role;
+import simple.mentoring.dto.user.UserSignupDto;
+import simple.mentoring.repository.UserRepository;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback(value = false)
 class UserServiceTest {
-  
+
+    @Autowired UserService userService;
+    @Autowired UserRepository userRepository;
+
+    private UserSignupDto userSignupDto;
+
+    @BeforeEach
+    public void setUp() {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+        userSignupDto = UserSignupDto.builder()
+                .loginId("testId")
+                .password(encoder.encode("1234"))
+                .email("test@email.com")
+                .nickname("testName")
+                .phone("01012345678")
+                .qualification(Qualification.MENTEE)
+                .build();
+    }
+
+    @Test
+    public void joinTest() throws Exception {
+        //given
+
+        //when
+        Long savedId = userService.join(userSignupDto);
+        //then
+        assertThat(userRepository.findById(savedId).get().getLoginId()).isEqualTo(userSignupDto.getLoginId());
+        assertThat(userRepository.findById(savedId).get().getNickname()).isEqualTo(userSignupDto.getNickname());
+        assertThat(userRepository.findById(savedId).get().getQualification()).isEqualTo(userSignupDto.getQualification());
+        assertThat(userRepository.findById(savedId).get().getRole()).isEqualTo(Role.USER);
+    }
 }
