39027622886e39a15f2b7fff92ca85fc03cb6a9e
jjdone <ehdlfaos@gmail.com>
2023-03-08 15:40:22 +0900

    Add implements OAuth2User

--- src/main/java/simple/mentoring/config/auth/PrincipalDetails.java
+++ src/main/java/simple/mentoring/config/auth/PrincipalDetails.java
@@ -3,17 +3,30 @@ package simple.mentoring.config.auth;
 import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import simple.mentoring.domain.User;
 
 import java.util.Collection;
+import java.util.Map;
 
 @Data
-public class PrincipalDetails implements UserDetails {
+public class PrincipalDetails implements UserDetails, OAuth2User {
 
     private User user;
+    private Map<String, Object> attributes;
 
     public PrincipalDetails(User user) {
         this.user = user;
+    }
+
+    public PrincipalDetails(User user, Map<String, Object> attributes) {
+        this.user = user;
+        this.attributes = attributes;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
     }
 
     @Override
@@ -49,5 +62,10 @@ public class PrincipalDetails implements UserDetails {
     @Override
     public boolean isEnabled() {
         return true;
+    }
+
+    @Override
+    public String getName() {
+        return (String) attributes.get("sub");
     }
 }
