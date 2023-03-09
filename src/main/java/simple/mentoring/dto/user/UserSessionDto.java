f8fc91793a62590f424ca6f20c0e3c6017b33e02
jjdone <ehdlfaos@gmail.com>
2023-03-09 14:37:55 +0900

    feat: add user session DTO

--- src/main/java/simple/mentoring/dto/user/UserSessionDto.java
+++ src/main/java/simple/mentoring/dto/user/UserSessionDto.java
@@ -1,2 +1,34 @@
-package simple.mentoring.dto.user;public class UserSessionDto {
+package simple.mentoring.dto.user;
+
+import lombok.Data;
+import simple.mentoring.domain.Qualification;
+import simple.mentoring.domain.Role;
+import simple.mentoring.domain.User;
+
+@Data
+public class UserSessionDto {
+
+    private String loginId;
+
+    private String password;
+
+    private String email;
+
+    private String nickname;
+
+    private Role role;
+
+    private Qualification qualification;
+
+    private String phone;
+
+    public UserSessionDto(User user) {
+        this.loginId = user.getLoginId();
+        this.password = user.getPassword();
+        this.email = user.getEmail();
+        this.nickname = user.getNickname();
+        this.role = user.getRole();
+        this.qualification = user.getQualification();
+        this.phone = user.getPhone();
+    }
 }
