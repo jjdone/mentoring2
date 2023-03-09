7bde76ee69f57e9af704e2653875d7f067b5d68f
jjdone <ehdlfaos@gmail.com>
2023-03-09 14:36:29 +0900

    feat: set login failure handler and login session

--- src/main/java/simple/mentoring/controller/MainController.java
+++ src/main/java/simple/mentoring/controller/MainController.java
@@ -4,21 +4,32 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 import simple.mentoring.domain.Qualification;
 import simple.mentoring.dto.user.UserLoginDto;
+import simple.mentoring.dto.user.UserSessionDto;
 import simple.mentoring.dto.user.UserSignupDto;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String mainForm() {
+    public String mainForm(HttpSession session, Model model) {
+        UserSessionDto user = (UserSessionDto) session.getAttribute("user");
+        if (user != null) {
+            model.addAttribute("user", user.getNickname());
+        }
         return "mainForm";
     }
 
     @GetMapping("/login")
-    public String loginForm(Model model) {
+    public String loginForm(@RequestParam(required = false) String error,
+                            @RequestParam(required = false) String exception,  Model model) {
         model.addAttribute("form", new UserLoginDto());
+        model.addAttribute("error", error);
+        model.addAttribute("exception", exception);
         return "loginForm";
     }
 
