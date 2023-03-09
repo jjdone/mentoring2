42223b2de4506bc807d261a65dd1689fd4beabcf
jjdone <ehdlfaos@gmail.com>
2023-03-09 14:33:14 +0900

    feat: add login data in session

--- src/main/java/simple/mentoring/config/auth/PrincipalDetailsService.java
+++ src/main/java/simple/mentoring/config/auth/PrincipalDetailsService.java
@@ -6,20 +6,25 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import simple.mentoring.domain.User;
+import simple.mentoring.dto.user.UserSessionDto;
 import simple.mentoring.repository.UserRepository;
+
+import javax.servlet.http.HttpSession;
 
 @Service
 @RequiredArgsConstructor
 public class PrincipalDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
+    private final HttpSession httpSession;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByLoginId(username);
-        if (user != null) {
-            return new PrincipalDetails(user);
-        }
-        return null;
+        User user = userRepository.findByLoginId(username).orElseThrow(() ->
+                new UsernameNotFoundException("해당 사용자가 존재하지 않습니다. : " + username));
+
+        httpSession.setAttribute("user", new UserSessionDto(user));
+
+        return new PrincipalDetails(user);
     }
 }
