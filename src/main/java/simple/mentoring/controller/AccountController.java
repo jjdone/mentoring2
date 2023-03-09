d01d0ca4ccadf992b771bb18a5b5d99511171679
jjdone <ehdlfaos@gmail.com>
2023-03-09 14:29:51 +0900

    feat: validation for signup
    if has error in sign up then model has errors and before form data

--- src/main/java/simple/mentoring/controller/AccountController.java
+++ src/main/java/simple/mentoring/controller/AccountController.java
@@ -1,21 +1,41 @@
 package simple.mentoring.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import simple.mentoring.domain.Qualification;
+import simple.mentoring.dto.user.UserLoginDto;
 import simple.mentoring.dto.user.UserSignupDto;
 import simple.mentoring.service.UserService;
 
+
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class AccountController {
 
     private final UserService userService;
 
     @PostMapping("/signup")
-    public String signup(@ModelAttribute UserSignupDto userSignupDto) {
+    public String signup(@Validated @ModelAttribute("form") UserSignupDto userSignupDto, BindingResult result, Model model) {
+
+        if (result.hasErrors()) {
+            log.info("errors={}", result);
+            model.addAttribute("form", userSignupDto);
+            return "signupForm";
+        }
+
         userService.join(userSignupDto);
         return "redirect:/login";
+    }
+
+    @ModelAttribute("qualification")
+    public Qualification[] qualifications() {
+        return Qualification.values();
     }
 }
