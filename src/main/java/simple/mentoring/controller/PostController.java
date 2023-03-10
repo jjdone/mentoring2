e3148b7449b322a9956278bc24223c65cd06635f
jjdone <ehdlfaos@gmail.com>
2023-03-10 20:41:59 +0900

    feat: Add PostController
    get: mainForm, uploadForm

--- src/main/java/simple/mentoring/controller/PostController.java
+++ src/main/java/simple/mentoring/controller/PostController.java
@@ -1,46 +1,41 @@
 package simple.mentoring.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestParam;
-import simple.mentoring.domain.Qualification;
-import simple.mentoring.dto.user.UserLoginDto;
+import simple.mentoring.dto.post.PostDto;
 import simple.mentoring.dto.user.UserSessionDto;
-import simple.mentoring.dto.user.UserSignupDto;
+import simple.mentoring.service.PostService;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Controller
-public class MainController {
+@RequiredArgsConstructor
+@Slf4j
+public class PostController {
+
+    private final PostService postService;
 
     @GetMapping("/")
     public String mainForm(HttpSession session, Model model) {
+
         UserSessionDto user = (UserSessionDto) session.getAttribute("user");
         if (user != null) {
             model.addAttribute("user", user.getNickname());
         }
+
+        List<PostDto> postDtoList = postService.findPostDtoList();
+        model.addAttribute("posts", postDtoList);
+
         return "mainForm";
     }
 
-    @GetMapping("/login")
-    public String loginForm(@RequestParam(required = false) String error,
-                            @RequestParam(required = false) String exception,  Model model) {
-        model.addAttribute("form", new UserLoginDto());
-        model.addAttribute("error", error);
-        model.addAttribute("exception", exception);
-        return "loginForm";
-    }
-
-    @GetMapping("/signup")
-    public String signupForm(Model model) {
-        model.addAttribute("form", new UserSignupDto());
-        return "signupForm";
-    }
-
-    @ModelAttribute("qualification")
-    public Qualification[] qualifications() {
-        return Qualification.values();
+    @GetMapping("/posts/uploadForm")
+    public String uploadForm(HttpSession session, Model model) {
+        model.addAttribute("form", new PostDto());
+        return "posts/uploadForm";
     }
 }
