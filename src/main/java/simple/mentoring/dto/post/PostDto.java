1e82f9effd9f19c45d2d4a5cd2161edf0ec53e46
jjdone <ehdlfaos@gmail.com>
2023-03-10 20:44:30 +0900

    feat: Add Post DTO
    feat: toEntity and toDto

--- src/main/java/simple/mentoring/dto/post/PostDto.java
+++ src/main/java/simple/mentoring/dto/post/PostDto.java
@@ -1,2 +1,35 @@
-package simple.mentoring.dto.post;public class PostListDto {
+package simple.mentoring.dto.post;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import simple.mentoring.domain.Post;
+import simple.mentoring.domain.User;
+
+@Data
+@NoArgsConstructor
+public class PostDto {
+
+    private Long id;
+    private Long userId;
+    private String title;
+    private String contents;
+    private String writer;
+
+    //Entity -> DTO
+    public PostDto(Post post) {
+        this.id = post.getId();
+        this.userId = post.getUser().getId();
+        this.title = post.getTitle();
+        this.contents = post.getContents();
+        this.writer = post.getWriter();
+    }
+
+    public Post toEntity(User user) {
+        return Post.builder()
+                .user(user)
+                .writer(user.getNickname())
+                .title(title)
+                .contents(contents)
+                .build();
+    }
 }
