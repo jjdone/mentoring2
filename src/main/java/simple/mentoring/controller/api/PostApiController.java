e7a688819750b588a0be2e66ed8253306fa68419
jjdone <ehdlfaos@gmail.com>
2023-03-10 20:40:42 +0900

    feat: post upload api

--- src/main/java/simple/mentoring/controller/api/PostApiController.java
+++ src/main/java/simple/mentoring/controller/api/PostApiController.java
@@ -1,2 +1,20 @@
-package simple.mentoring.controller.api;public class PostApiController {
+package simple.mentoring.controller.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import simple.mentoring.dto.post.PostDto;
+import simple.mentoring.service.PostService;
+
+@RequestMapping("/api")
+@RestController
+@RequiredArgsConstructor
+public class PostApiController {
+
+    private final PostService postService;
+
+    @PostMapping("posts/upload")
+    public ResponseEntity<?> upload(@ModelAttribute("form") PostDto postDto) {
+        return ResponseEntity.ok(postService.upload(postDto));
+    }
 }
