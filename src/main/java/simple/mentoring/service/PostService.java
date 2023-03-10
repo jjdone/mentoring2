dde05660293e2573e80b14bfe687870b9278c6ff
jjdone <ehdlfaos@gmail.com>
2023-03-10 20:47:41 +0900

    feat: Add PostService
    feat: upload post and find post DTO list

--- src/main/java/simple/mentoring/service/PostService.java
+++ src/main/java/simple/mentoring/service/PostService.java
@@ -1,2 +1,36 @@
-package simple.mentoring.service;public class PostService {
+package simple.mentoring.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import simple.mentoring.domain.Post;
+import simple.mentoring.domain.User;
+import simple.mentoring.dto.post.PostDto;
+import simple.mentoring.repository.PostRepository;
+import simple.mentoring.repository.UserRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class PostService {
+
+    private final PostRepository postRepository;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public Long upload(PostDto postDto) {
+        User user = userRepository.findByNickname(postDto.getWriter()).get();
+        Post post = postDto.toEntity(user);
+        return postRepository.save(post).getId();
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostDto> findPostDtoList() {
+        List<Post> posts = postRepository.findAll();
+        return posts.stream()
+                .map(PostDto::new)
+                .collect(Collectors.toList());
+    }
 }
