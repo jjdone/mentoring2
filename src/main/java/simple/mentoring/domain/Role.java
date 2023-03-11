50cf1e7a69555b37662ef8c7a71af8e904122665
jjdone <ehdlfaos@gmail.com>
2023-03-11 22:56:01 +0900

    fix: Set ROLE_ format

--- src/main/java/simple/mentoring/domain/Role.java
+++ src/main/java/simple/mentoring/domain/Role.java
@@ -6,8 +6,8 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum Role {
-    ADMIN("관리자"),
-    USER("사용자");
+    ROLE_ADMIN("관리자"),
+    ROLE_USER("사용자");
 
     private final String krRole;
 }
