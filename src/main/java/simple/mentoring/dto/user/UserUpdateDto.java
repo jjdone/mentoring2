838f68c29428d004a57d1d4acced2d649041aaca
jjdone <ehdlfaos@gmail.com>
2023-03-12 15:47:48 +0900

    feat: Add user update DTO

--- src/main/java/simple/mentoring/dto/user/UserUpdateDto.java
+++ src/main/java/simple/mentoring/dto/user/UserUpdateDto.java
@@ -1,2 +1,18 @@
-package simple.mentoring.dto.user;public class UserUpdateDto {
+package simple.mentoring.dto.user;
+
+import lombok.Data;
+import simple.mentoring.domain.User;
+
+@Data
+public class UserUpdateDto {
+
+    private String password;
+    private String nickname;
+    private String phone;
+
+    public UserUpdateDto(User user) {
+        this.password = user.getPassword();
+        this.nickname = user.getNickname();
+        this.phone = user.getPhone();
+    }
 }
