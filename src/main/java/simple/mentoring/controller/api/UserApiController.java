1ff5cbae8cecd0c2d11bedc9aa6a6d8e968d2119
jjdone <ehdlfaos@gmail.com>
2023-03-13 23:54:36 +0900

    feat: Add update function

--- src/main/java/simple/mentoring/controller/api/UserApiController.java
+++ src/main/java/simple/mentoring/controller/api/UserApiController.java
@@ -1,2 +1,20 @@
-package simple.mentoring.controller.api;public class UserApiContorller {
+package simple.mentoring.controller.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import simple.mentoring.dto.user.UserUpdateDto;
+import simple.mentoring.service.UserService;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserApiController {
+
+    private final UserService userService;
+
+    @PostMapping("/{userId}/update")
+    public ResponseEntity<?> update(@PathVariable Long userId, @ModelAttribute UserUpdateDto userUpdateDto) {
+        return ResponseEntity.ok(userService.update(userUpdateDto));
+    }
 }
