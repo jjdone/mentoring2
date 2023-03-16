8cebd8627b55a6b0e0963614f436c546c4fb02f9
jjdone <ehdlfaos@gmail.com>
2023-03-16 17:44:07 +0900

    feat: Add post update DTO

--- src/main/java/simple/mentoring/dto/post/PostUpdateDto.java
+++ src/main/java/simple/mentoring/dto/post/PostUpdateDto.java
@@ -1,2 +1,18 @@
-package simple.mentoring.dto.post;public class PostUpdateDto {
+package simple.mentoring.dto.post;
+
+import lombok.Data;
+import simple.mentoring.domain.Post;
+
+@Data
+public class PostUpdateDto {
+
+    private Long id;
+    private String title;
+    private String contents;
+
+    public PostUpdateDto(Post post) {
+        this.id = post.getId();
+        this.title = post.getTitle();
+        this.contents = post.getContents();
+    }
 }
